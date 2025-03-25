286b22d02c6e0aec4cb517a606d9f12b818839de
Rashini Dias <diasrd.22@uom.lk>
2025-03-25 12:10:32 +0530

    The new exploration log

---
+++ adventure-log.py
@@ -0,0 +1,12 @@
+# adventure_log.py
+
+def log_discovery(feature, description):
+    print(f"GitHub Log: You unlocked '{feature}'!")
+    print(f"{description}\n")
+
+#MyDiscoveries
+log_discovery("The Repository", "A magical place to safely store the code.")
+log_discovery("Branches", "Secret paths to experiment!")
+log_discovery("Commit Messages", "Ancient spells to record the journey.")
+log_discovery("Merge Conflicts", "No conflicts? just discover everything in one place.")
+log_discovery("Pull Requests", "Friendly invitations for my fellow explorers!")
