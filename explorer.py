b72420e5dfa8eee5d1c6e3a575a4a5e3fc295055
Rashini Dias <diasrd.22@uom.lk>
2025-03-25 11:46:09 +0530

    Discovered a hidden website

--- explorer.py
+++ explorer.py
@@ -1,1 +1,3 @@
 print("Welcome, Explorer! Your journey begins now!")
+print("Hand on tight this would be just the beginning of a new journey!")
+print("You've discovered a hidden website with a cat in the black background!")
