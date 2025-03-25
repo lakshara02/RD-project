6bba24e03ad2fd77453ad3614f3bf2ffeb722986
lakshara02 <lakshara.lk@gmail.com>
2025-03-25 20:10:27 +0530

    Update adventure-log.py

--- adventure-log.py
+++ adventure-log.py
@@ -11,3 +11,8 @@ log_discovery("Commit Messages", "Ancient spells to record the journey.")
 log_discovery("Merge Conflicts", "No conflicts? just discover everything in one place.")
 log_discovery("Pull Requests", "Friendly invitations for my fellow explorers!")
 log_discovery("GitHub Issues", "Let's solve them on the way!")
+
+def log_discovery(feature, description):
+    print(f"GitHub Log: You unlocked '{feature}'!")
+    print(f"{description}\n")
+log_discovery("Collaboration", "Working together magically enhances your repository!")
